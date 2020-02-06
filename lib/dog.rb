122c51f70652ebb90dd6b77ea6d914c761e9b8b9
Hailey Bongo <haileyebongo@gmail.com>
2020-02-06 04:45:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,14 +12,14 @@ class Dog
     @name
   end
  
-
+  def breed=(breed)
+    @breed = breed
+  end
   
   def breed
     @breed
   end
   
-  def breed=(new_breed)
-    @breed = new_breed
-  end
+
  
 end
